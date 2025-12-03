26f96a3b5df9143ea9ebfc0ad52e663a96c11d4b
Rone <Gamewarrior1andbowser@gmail.com>
2025-12-03 10:00:30 -0600

    Imported map from GPS example, but need to debug it...

---
+++ assets/script/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VybGlua2F1ciIsImEiOiJjbHExYjM4cHUwNzE3MnBud25qNDlmc2VjIn0.Jeu9BD0h1vILAwXce8dQqw';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    zoom: 17,
+    pitch: 40
+});
+
+const marker = new mapboxgl.Marker({color: '#ff7342'});
+
+const options = {
+    enableHighAccuracy: true
+}
+
+function getLocation(position) {
+    let { latitude, longitude } = position.coords;
+    map.setCenter([longitude, latitude]);
+    marker.setLngLat([longitude, latitude]).addTo(map);
+}
+
+function errorHandler() {
+    console.log('Unable to retrieve your location');
+}
+
+function disabledOptions() {
+    map.dragPan.disable();
+    map.keyboard.disable();
+    map.scrollZoom.disable();
+    map.doubleClickZoom.disable();
+    map.touchZoomRotate.disable();
+}
+
+function displayPosition() {
+    if('geolocation' in navigator) {
+        navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+        disabledOptions();
+    } else {
+        console.log('Geolocation is not supported by your browser.');
+    }
+}
+
+displayPosition();
